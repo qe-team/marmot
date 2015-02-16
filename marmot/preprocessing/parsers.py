263b743628c75e4b6869b3a966af67f051d7fe91
Chris Hokamp <chris.hokamp@gmail.com>
2015-02-16 16:56:01 +0000

    copied parsers from simple branch

--- marmot/preprocessing/parsers.py
+++ marmot/preprocessing/parsers.py
@@ -7,10 +7,11 @@ from marmot.util.simple_corpus import SimpleCorpus
 from collections import defaultdict, Counter
 from subprocess import Popen, call
 from nltk import word_tokenize
+import time
+import os, sys, errno
+
 from marmot.util.force_align import Aligner
 from marmot.util.alignments import train_alignments
-import time
-import os, sys
 
 
 def extract_important_tokens(corpus_file, min_count=1):
@@ -72,6 +73,7 @@ def list_of_contexts_additional(contexts, representations, repr_labels, interest
 
 
 # generate a unique random string
+# used for temporary file creation and deletion
 def get_random_name(prefix=''):
     return 'tmp'+prefix+str(time.time())
 
@@ -122,7 +124,7 @@ def get_pos_tagging( src_file, tagger, par_file, label ):
 
     return (label, output)
 
-
+# TODO: this is an additional representation - requires the alignment files to exist
 def force_alignments(src_file, tg_file, trained_model):
     alignments = []
     aligner = Aligner(trained_model+'.fwd_params',trained_model+'.fwd_err',trained_model+'.rev_params',trained_model+'.rev_err')
@@ -144,6 +146,7 @@ def force_alignments(src_file, tg_file, trained_model):
  
 
 # force alignment with fastalign
+# TODO: commented because there is a duplicate function in simple-->parsers.py
 # if no alignment model provided, builds the alignment model first
 # <align_model> - path to alignment model such that <align_model>.frd_params, .rev_params, .fwd_err, .rev_err exist
 # <src_file>, <tg_file> - files to be aligned
@@ -152,21 +155,22 @@ def force_alignments(src_file, tg_file, trained_model):
 #      [ ....                                           ]
 #        ....
 #      [ ....                                           ] ]
-def get_alignments(src_file, tg_file, trained_model = None, src_train='', tg_train='', align_model = 'align_model', label='alignments'):
-    alignments = []
-    print "Get alignments"
-    if trained_model == None: 
-        trained_model = train_alignments(src_train, tg_train, align_model)
-        if trained_model == '':
-            sys.stderr.write('No alignment model trained\n')
-            return []
+# def get_alignments(src_file, tg_file, trained_model = None, src_train='', tg_train='', align_model = 'align_model', label='alignments'):
+#     alignments = []
+#     print "Get alignments"
+#     if trained_model == None:
+#         trained_model = train_alignments(src_train, tg_train, align_model)
+#         if trained_model == '':
+#             sys.stderr.write('No alignment model trained\n')
+#             return []
+#
+#     print 'Trained model: ', trained_model
+#     alignments = force_alignments( src_file, tg_file, trained_model )
+#
+#     return (label, alignments)
 
-    print 'Trained model: ', trained_model
-    alignments = force_alignments( src_file, tg_file, trained_model )
 
-    return (label, alignments)
-
-
+# get all of the bad contexts in a list of contexts
 def list_of_bad_contexts(contexts, labels, interesting_tokens=None):
     token_contexts = []
     for doc in contexts:
@@ -185,6 +189,148 @@ def parse_corpus_contexts(corpus_file, interesting_tokens=None, tag=1):
 def get_corpus_file(corpus_file, label):
     corpus = SimpleCorpus(corpus_file)
     return (label, corpus.get_texts())
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
+# convert WMT file to two files: plain text and labels
+#    plain text:  word word word word word
+#    labels:      OK   BAD  BAD  OK   OK
+# check that source and target have the same number of sentences, re-write source file without sentence ids
+def parse_wmt_to_text(wmt_file, wmt_source_file):
+    tmp_dir = os.getcwd()+'/tmp_dir'
+    mkdir_p(tmp_dir)
+
+    target_file = tmp_dir+'/'+os.path.basename(wmt_file)+'.target'
+    tags_file = tmp_dir+'/'+os.path.basename(wmt_file)+'.tags'
+    source_file = tmp_dir+'/'+os.path.basename(wmt_source_file)+'.txt'
+
+    target = open(target_file, 'w')
+    tags = open(tags_file, 'w')
+    source = open(source_file, 'w')
+    cur_num = None
+    cur_sent, cur_tags = [], []
+
+    # parse source files
+    source_sents = {}
+    for line in open(wmt_source_file):
+        str_num = line.decode('utf-8').strip().split('\t')
+        source_sents[str_num[0]] = str_num[1]
+
+    # parse target file and write new source, target, and tag files
+    for line in open(wmt_file):
+        chunks = line[:-1].decode('utf-8').split('\t')
+        if chunks[0] != cur_num:
+            if cur_num is not None:
+                # check that the sentence is in source
+                if cur_num in source_sents:
+                    source.write('%s\n' % source_sents[cur_num].encode('utf-8'))
+                    target.write('%s\n' % (' '.join([w.encode('utf-8') for w in cur_sent])))
+                    tags.write('%s\n' % (' '.join([w.encode('utf-8') for w in cur_tags])))
+                cur_sent = []
+                cur_tags = []
+            cur_num = chunks[0]
+        cur_sent.append(chunks[2])
+        cur_tags.append(chunks[5])
+    # last sentence
+    if len(cur_sent) > 0 and cur_num in source_sents:
+        source.write('%s\n' % source_sents[cur_num].encode('utf-8'))
+        target.write('%s\n' % (' '.join([w.encode('utf-8') for w in cur_sent])))
+        tags.write('%s\n' % (' '.join([w.encode('utf-8') for w in cur_tags])))
+
+    tags.close()
+    target.close()
+    source.close()
+
+    return {'target': target_file, 'source': source_file, 'tag': tags_file}
+
+
+
+
+def get_corpus(target_file, source_file, tag):
+    return {'target': target_file, 'source': source_file, 'tag': tag}
+
+
+def cur_dir():
+    import os
+    print(os.getcwd())
+
+# copy of the function from parsers.py, but it writes the tagging to the file
+# TODO: this is specifically for tree tagger, it's not a general pos tagging function
+# TODO: this function should be a preprocessing option -- it's actually a representation generator
+# and returns the filename
+def get_pos_tagging(src_file, tagger, par_file, label):
+    print("Start tagging", src_file)
+    # tokenize and add the sentence end marker
+    # tokenization is done with nltk
+    tmp_tokenized_name = get_random_name(prefix='_tok')
+    tmp_tok = open(tmp_tokenized_name, 'wr+')
+    for line in open(src_file):
+        words = word_tokenize(line[:-1].decode('utf-8'))
+        tmp_tok.write('%s\nSentenceEndMarker\n' % '\n'.join([w.encode('utf-8') for w in words]))
+    tmp_tok.seek(0)
+
+    # pass to tree-tagger
+    tmp_tagged_name = get_random_name(prefix='_tag')
+    tmp_tagged = open(tmp_tagged_name, 'wr+')
+    tagger_call = Popen([tagger, '-token', par_file], stdin=tmp_tok, stdout=tmp_tagged)
+    tagger_call.wait()
+    tmp_tagged.seek(0)
+
+    # remove sentence markers, restore sentence structure
+    tmp_final_name = get_random_name(prefix='_final')
+    tmp_final = open(tmp_final_name, 'w')
+    output = []
+    cur_sentence = []
+    for line in tmp_tagged:
+        word_tag = line[:-1].decode('utf-8').strip().split('\t')
+        # each string has to be <word>\t<tag>
+        if len(word_tag) != 2:
+            continue
+        if word_tag[0] == 'SentenceEndMarker':
+            tmp_final.write('%s\n' % ' '.join([tag.encode('utf-8') for tag in cur_sentence]))
+            output.append(cur_sentence)
+            cur_sentence = []
+        else:
+            cur_sentence.append(word_tag[1])
+    tmp_tok.close()
+    tmp_tagged.close()
+    tmp_final.close()
+
+    # delete all temporary files
+    call(['rm', tmp_tokenized_name, tmp_tagged_name])
+
+    return (label, tmp_final_name)
+
+
+# copy of the function from parsers.py, but it writes the tagging to the file
+# and returns the filename
+def get_alignments(src_file, tg_file, trained_model=None, src_train='', tg_train='', align_model='align_model', label='alignments'):
+    if trained_model is None:
+        trained_model = train_alignments(src_train, tg_train, align_model)
+        if trained_model == '':
+            sys.stderr.write('No alignment model trained\n')
+            return []
+
+    aligner = Aligner(trained_model+'.fwd_params', trained_model+'.fwd_err', trained_model+'.rev_params', trained_model+'.rev_err')
+    src = open(src_file)
+    tg = open(tg_file)
+    align_file = src_file+'_'+os.path.basename(tg_file)+'.aligned'
+    aligned = open(align_file, 'w')
+    for src_line, tg_line in zip(src, tg):
+        aligned.write(aligner.align(src_line[:-1].decode('utf-8')+u' ||| '+tg_line[:-1].decode('utf-8'))+u'\n')
+    aligned.close()
+    aligner.close()
+
+    return (label, align_file)
+
+
 
 # parse_corpus_contexts with additional representations
 # <additional> list of representations, each of format (<label>, <generator>)
@@ -206,6 +352,8 @@ def parse_back_translation(corpus_file, labels_file, interesting_tokens=None):
 
 from itertools import groupby
 import codecs
+
+
 # matching sentences may require us to include the sen id
 # if source is not provided, pass an empty string ('') as <source_file>
 def parse_wmt14_data(corpus_file, source_file, interesting_tokens=None):
