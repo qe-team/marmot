14af72e47d905f766a6e8c33c338233479ed92d6
varvara_k <varvara_k@bitbucket.org>
2015-02-19 13:17:01 +0000

    backoff behavior feature and tests for it

--- marmot/features/lm_feature_extractor.py
+++ marmot/features/lm_feature_extractor.py
@@ -28,13 +28,35 @@ class LMFeatureExtractor(FeatureExtractor):
                 return i
         return 0
 
+    def get_backoff(self, ngram):
+        assert(len(ngram) == 3)
+        ngram = tuple(ngram)
+        if ngram in self.lm[2]:
+            return 1.0
+        elif ngram[:2] in self.lm[1] and ngram[1:] in self.lm[1]:
+            return 0.8
+        elif ngram[1:] in self.lm[1]:
+            return 0.6
+        elif ngram[:2] in self.lm[1] and ngram[2] in self.lm[0]:
+            return 0.4
+        elif ngram[1] in self.lm[0] and ngram[2] in self.lm[0]:
+            return 0.3
+        elif ngram[2] in self.lm[0]:
+            return 0.2
+        else:
+            return 0.1
+
     # returns a set of features related to LM
     # currently extracting: highest order ngram including the word and its LEFT context,
     #                       highest order ngram including the word and its RIGHT context
     def get_features(self, context_obj):
-        left_ngram = self.check_lm(context_obj['target'][:context_obj['index']+1], side='left')
-        right_ngram = self.check_lm(context_obj['target'][context_obj['index']:], side='right')
-        return [left_ngram, right_ngram]
+        idx = context_obj['index']
+        left_ngram = self.check_lm(context_obj['target'][:idx+1], side='left')
+        right_ngram = self.check_lm(context_obj['target'][idx:], side='right')
+        backoff_left = self.get_backoff(context_obj['target'][idx-2:idx+1])
+        backoff_middle = self.get_backoff(context_obj['target'][idx-1:idx+2])
+        backoff_right = self.get_backoff(context_obj['target'][idx:idx+3])
+        return [left_ngram, right_ngram, backoff_left, backoff_middle, backoff_right]
 
     def get_feature_names(self):
-        return ['highest_order_ngram_left', 'highest_order_ngram_right']
+        return ['highest_order_ngram_left', 'highest_order_ngram_right', 'backoff_behavior_left', 'backoff_behavior_middle', 'backoff_behavior_right']
