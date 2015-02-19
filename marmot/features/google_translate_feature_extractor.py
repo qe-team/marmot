dc63ad4c112ace7aead9759cf02dcf67cdfd9e1f
varvara_k <varvara_k@bitbucket.org>
2015-02-19 16:27:26 +0000

    generate pseudo-reference representation with google translate

--- marmot/features/google_translate_feature_extractor.py
+++ marmot/features/google_translate_feature_extractor.py
@@ -1,5 +1,6 @@
 from goslate import Goslate
 from nltk import word_tokenize
+import ipdb
 
 from marmot.features.feature_extractor import FeatureExtractor
 
@@ -7,17 +8,21 @@ from marmot.features.feature_extractor import FeatureExtractor
 class GoogleTranslateFeatureExtractor(FeatureExtractor):
 
     def __init__(self, lang='en'):
-        self.gs = Goslate()
         self.lang = lang
 
     def get_features(self, context_obj):
         if 'source' not in context_obj:
             print("No source for translation")
             return []
-        translation = word_tokenize(self.gs.translate(' '.join(context_obj['source']), self.lang))
+        
+        if 'pseudo-reference' in context_obj:
+            translation = context_obj['pseudo-reference']
+        else:
+            gs = Goslate()
+            translation = word_tokenize(gs.translate(' '.join(context_obj['source']), self.lang))
         if context_obj['token'] in translation:
             return [1]
         return [0]
 
     def get_feature_names(self):
-        return ["pseudo_reference"]
+        return ["pseudo-reference"]
