82633dad3ecd928196b2bf429381344ea3cac9fb
varvara_k <varvara_k@bitbucket.org>
2015-02-19 17:19:09 +0000

    source polysemy count added

--- marmot/features/tests/test_wordnet_feature_extractor.py
+++ marmot/features/tests/test_wordnet_feature_extractor.py
@@ -5,14 +5,33 @@ from marmot.features.wordnet_feature_extractor import WordnetFeatureExtractor
 class WordnetFeatureExtractorTests(unittest.TestCase):
 
     def setUp(self):
-        self.wordnet_extractor = WordnetFeatureExtractor()
-        self.wordnet_extractor_fr = WordnetFeatureExtractor()
-        pass
+        self.wordnet_extractor = WordnetFeatureExtractor(src_lang='fre', tg_lang='en')
+#        self.wordnet_extractor_fr = WordnetFeatureExtractor()
 
-    def test_no_pos_eng(self):
+    def test_get_features(self):
         obj = {'token':u'hits', 'index':2, 'target':[u'a',u'boy',u'hits',u'a',u'dog'], 'source':[u'un', u'garcon',u'frappe', u'un', u'chien'], 'target_pos':['DT','NN','VBZ', 'DT', 'NN'], 'source_pos':['DT','NN','VBZ', 'DT', 'NN'], 'alignments':[[0],[1],[3],[2],[4]]}
-        wn_feature = self.wordnet_extractor.get_features(obj)
-        self.assertEqual(wn_feature, 24)
+        wn_src, wn_tg = self.wordnet_extractor.get_features(obj)
+        self.assertEqual(wn_src, 9)
+        self.assertEqual(wn_tg, 24)
+
+    def test_no_source(self):
+        obj = {'token':u'hits', 'index':2, 'target':[u'a',u'boy',u'hits',u'a',u'dog'], 'target_pos':['DT','NN','VBZ', 'DT', 'NN']}
+        wn_src, wn_tg = self.wordnet_extractor.get_features(obj)
+        self.assertEqual(wn_src, 0)
+        self.assertEqual(wn_tg, 24)
+
+    def test_no_alignment(self):
+        obj = {'token':u'hits', 'index':2, 'target':[u'a',u'boy',u'hits',u'a',u'dog'], 'source':[u'un', u'garcon',u'frappe', u'un', u'chien'], 'target_pos':['DT','NN','VBZ', 'DT', 'NN'], 'source_pos':['DT','NN','VBZ', 'DT', 'NN']}
+        wn_src, wn_tg = self.wordnet_extractor.get_features(obj)
+        self.assertEqual(wn_src, 0)
+        self.assertEqual(wn_tg, 24)
+
+    def test_multi_alignment(self):
+        obj = {'token':u'hits', 'index':2, 'target':[u'a',u'boy',u'hits',u'a',u'dog'], 'source':[u'un', u'garcon',u'frappe', u'un', u'chien'], 'target_pos':['DT','NN','VBZ', 'DT', 'NN'], 'source_pos':['DT','NN','VBZ', 'DT', 'NN'], 'alignments':[[0],[1],[3, 4],[2],[4]]}
+        wn_src, wn_tg = self.wordnet_extractor.get_features(obj)
+        self.assertEqual(wn_src, 9)
+        self.assertEqual(wn_tg, 24)
+
 
 #    def test_no_pos_fr(self):
 if __name__ == '__main__':
