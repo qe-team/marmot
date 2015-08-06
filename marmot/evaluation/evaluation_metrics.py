f012cdbe8e5ed214dd9b9129d8d587fb0e6b567e
varvara-l <varvara.logacheva@gmail.com>
2015-08-06 10:40:36 +0100

    weighted sequence correlation

--- marmot/evaluation/evaluation_metrics.py
+++ marmot/evaluation/evaluation_metrics.py
@@ -67,7 +67,33 @@ def intersect_spans(true_span, pred_span):
 
 
 # Y_true and y_pred - lists of sequences
-def sequence_correlation(y_true, y_pred, good_label=1, bad_label=0):
+#def sequence_correlation(y_true, y_pred, good_label=1, bad_label=0):
+#    assert(len(y_true) == len(y_pred))
+#    if not list_of_lists(y_true) and not list_of_lists(y_pred):
+#        logger.warning("You provided the labels in a flat list of length {}. Assuming them to be one sequence".format(len(y_true)))
+#        y_true = [y_true]
+#        y_pred = [y_pred]
+#    elif list_of_lists(y_true) and list_of_lists(y_pred):
+#        pass
+#    else:
+#        logger.error("Shapes of the hypothesis and the reference don't match")
+#        return 0
+#
+#    sentence_pred = []
+#    for true_sent, pred_sent in zip(y_true, y_pred):
+#        assert(len(true_sent) == len(pred_sent))
+#        true_spans_1, true_spans_0 = get_spans(true_sent, good_label=good_label, bad_label=bad_label)
+#        pred_spans_1, pred_spans_0 = get_spans(pred_sent, good_label=good_label, bad_label=bad_label)
+#
+#        res_1 = intersect_spans(true_spans_1, pred_spans_1)
+#        res_0 = intersect_spans(true_spans_0, pred_spans_0)
+#
+#        sentence_pred.append((res_1+res_0)/len(true_sent))
+#
+#    return sentence_pred, np.average(sentence_pred)
+
+# Y_true and y_pred - lists of sequences
+def sequence_correlation(y_true, y_pred, good_label=1, bad_label=0, out='sequence_corr.out', verbose=False):
     assert(len(y_true) == len(y_pred))
     if not list_of_lists(y_true) and not list_of_lists(y_pred):
         logger.warning("You provided the labels in a flat list of length {}. Assuming them to be one sequence".format(len(y_true)))
@@ -80,6 +106,8 @@ def sequence_correlation(y_true, y_pred, good_label=1, bad_label=0):
         return 0
 
     sentence_pred = []
+    if verbose:
+        out_file = open(out, 'w')
     for true_sent, pred_sent in zip(y_true, y_pred):
         assert(len(true_sent) == len(pred_sent))
         true_spans_1, true_spans_0 = get_spans(true_sent, good_label=good_label, bad_label=bad_label)
@@ -88,6 +116,70 @@ def sequence_correlation(y_true, y_pred, good_label=1, bad_label=0):
         res_1 = intersect_spans(true_spans_1, pred_spans_1)
         res_0 = intersect_spans(true_spans_0, pred_spans_0)
 
-        sentence_pred.append((res_1+res_0)/len(true_sent))
+        corr_val = (res_1+res_0)/float(len(true_sent))
+#        print(corr_val, type(corr_val))
+        if verbose:
+            out_file.write("Reference:  %s\nPrediction: %s\nCorrelation: %s\n" % (' '.join([str(t) for t in true_sent]), ' '.join([str(t) for t in pred_sent]), str(corr_val)))
+        sentence_pred.append(corr_val)
 
+    if verbose:
+        out_file.close()
+    return sentence_pred, np.average(sentence_pred)
+
+
+def sequence_correlation_weighted(y_true, y_pred, good_label=1, bad_label=0, out='sequence_corr.out', verbose=False):
+    assert(len(y_true) == len(y_pred))
+    if not list_of_lists(y_true) and not list_of_lists(y_pred):
+        logger.warning("You provided the labels in a flat list of length {}. Assuming them to be one sequence".format(len(y_true)))
+        y_true = [y_true]
+        y_pred = [y_pred]
+    elif list_of_lists(y_true) and list_of_lists(y_pred):
+        pass
+    else:
+        logger.error("Shapes of the hypothesis and the reference don't match")
+        return 0
+
+    sentence_pred = []
+    if verbose:
+        out_file = open(out, 'w')
+    for true_sent, pred_sent in zip(y_true, y_pred):
+        ref_bad = sum([1 for l in true_sent if l == bad_label])
+        ref_good = sum([1 for l in true_sent if l == good_label])
+        assert(ref_bad + ref_good == len(true_sent))
+        # coefficients that ensure the equal influence of good and bad classes on the overall score
+        try:
+            coeff_bad = len(true_sent)/(2*ref_bad)
+        except ZeroDivisionError:
+            coeff_bad = 0.0
+        try:
+            coeff_good = len(true_sent)/(2*ref_good)
+        except ZeroDivisionError:
+            coeff_good = 0.0
+
+        assert(len(true_sent) == len(pred_sent))
+        true_spans_1, true_spans_0 = get_spans(true_sent, good_label=good_label, bad_label=bad_label)
+        pred_spans_1, pred_spans_0 = get_spans(pred_sent, good_label=good_label, bad_label=bad_label)
+
+        res_1 = intersect_spans(true_spans_1, pred_spans_1)
+        res_0 = intersect_spans(true_spans_0, pred_spans_0)
+
+        len_t_1, len_t_0 = len(true_spans_1), len(true_spans_0)
+        len_p_1, len_p_0 = len(pred_spans_1), len(pred_spans_0)
+        if len_t_1 + len_t_0 > len_p_1 + len_p_0:
+            spans_ratio = (len_p_1 + len_p_0)/(len_t_1 + len_t_0)
+        else:
+            spans_ratio = (len_t_1 + len_t_0)/(len_p_1 + len_p_0)
+
+        corr_val = (res_1*coeff_good + res_0*coeff_bad)*spans_ratio/float(len(true_sent))
+#        try:
+#            corr_val = res_0/float(ref_bad)
+#        except ZeroDivisionError:
+#            corr_val = 1.0
+#        print(corr_val, type(corr_val))
+        if verbose:
+            out_file.write("Reference:  %s\nPrediction: %s\nCorrelation: %s\n" % (' '.join([str(t) for t in true_sent]), ' '.join([str(t) for t in pred_sent]), str(corr_val)))
+        sentence_pred.append(corr_val)
+
+    if verbose:
+        out_file.close()
     return sentence_pred, np.average(sentence_pred)
