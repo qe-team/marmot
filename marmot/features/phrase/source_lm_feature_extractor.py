2f264ae37522f3684c48403ab179e73822807233
varvara-l <varvara.logacheva@gmail.com>
2015-09-01 17:54:36 +0100

    bugfix

--- marmot/features/phrase/source_lm_feature_extractor.py
+++ marmot/features/phrase/source_lm_feature_extractor.py
@@ -8,8 +8,6 @@ class SourceLMFeatureExtractor(FeatureExtractor):
         self.model = kenlm.LanguageModel(lm_file)
 
     def get_features(self, context_obj):
-        if 'source_token' not in context_obj or len(context_obj['source_token'] == 0):
-            return 0
         return self.model.score(' '.join(context_obj['source_token']))
 
     def get_feature_names(self):
