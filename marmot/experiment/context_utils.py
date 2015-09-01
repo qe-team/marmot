54154ded3ae7b79659e3483a1f97fb7e321f5cc7
varvara-l <varvara.logacheva@gmail.com>
2015-09-01 11:28:04 +0100

    compute source phrase from alignments in the context object

--- marmot/experiment/context_utils.py
+++ marmot/experiment/context_utils.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from __future__ import print_function, division
 
 import sys
 import numpy as np
@@ -28,7 +28,7 @@ def negative_window(my_list, start, end):
     return res
 
 
-def create_context_ngram(repr_dict, order, test=False, all_bad=False):
+def create_context_ngram(repr_dict, order, test=False, only_target=False, bad_tagging="pessimistic"):
     '''
     :param repr_dict: a dict representing a 'line' or 'sentence' or a 'segment'
     :return: a list of context objects representing the data for each token in the sequence
@@ -72,10 +72,11 @@ def create_context_ngram(repr_dict, order, test=False, all_bad=False):
 
 # we don't really need the order here, it should always be None
 # or anything else
-# :test: -- True if data is test data, False if training
-# :all_bad: -- tag all phrases with at least one bad word as "BAD"
+# :test: -- True if data is test data, False if training -- test sentences can have empty source-segmentation field (if Moses failed to produce constrained reference for them)
+# :only_target: -- True if only target sentence is segmented, needs to be processed without source segmentation
+# :bad_tagging: -- tag all phrases with at least one bad word as "BAD"
 #              if seg to False - only phrases with 50% or more bad words are tagged as "BAD"
-def create_context_phrase(repr_dict, order=None, test=False, all_bad=False):
+def create_context_phrase(repr_dict, order=None, test=False, only_target=False, bad_tagging="pessimistic"):
     '''
     :param repr_dict: a dict representing a 'line' or 'sentence' or a 'segment'
     :return: a list of context objects representing the data for each token in the sequence
@@ -90,65 +91,84 @@ def create_context_phrase(repr_dict, order=None, test=False, all_bad=False):
         print("No 'tag' label in data representations or wrong format of tag")
         print(repr_dict)
         return []
-    if 'segmentation' not in repr_dict:
+    if 'segmentation' not in repr_dict or len(repr_dict['segmentation']) == 0:
         print("No 'segmentation' label in data representations")
         return []
 
-    if len(repr_dict['segmentation']) == 0:
-        print("No segmentation: ", repr_dict)
     # no source segmentation means that no Moses segmentation was produced
     # in the training data we leave these sentences out
     # in the test data they are processed as normal
     # assuming that every target word is a separate segment
-    if 'source_segmentation' not in repr_dict or len(repr_dict['source_segmentation']) == 0:
-        if test:
-            print("Test sentence with no source segmentation")
-        else:
-            print("Sentence withdrawn: ", repr_dict)
-            return []
+    if ('source_segmentation' not in repr_dict or len(repr_dict['source_segmentation']) == 0) and not test and not only_target:
+        return []
     active_keys = repr_dict.keys()
     active_keys.remove('tags')
     if 'source_segmentation' in repr_dict:
         active_keys.remove('source_segmentation')
+        if len(repr_dict['source_segmentation']) != len(repr_dict['segmentation']) and not test:
+            print("Wrong segmentation lengths: ", repr_dict)
+            sys.exit()
     active_keys.remove('segmentation')
-    if len(repr_dict['source_segmentation']) != len(repr_dict['segmentation']) and not test:
-        print("Wrong segmentation lengths: ", repr_dict)
-        sys.exit()
-    print("Generating the contexts for sentence, {} segments".format(len(repr_dict['segmentation'])))
-    print(repr_dict)
     for idx, (i, j) in enumerate(repr_dict['segmentation']):
 
         c = {}
         c['token'] = repr_dict['target'][i:j]
+        c['index'] = [i, j]
+        # source phrase from the phrase segmentation
         if 'source_segmentation' in repr_dict and len(repr_dict['source_segmentation']) != 0:
             src_seg = repr_dict['source_segmentation'][idx]
             c['source_token'] = repr_dict['source'][src_seg[0]:src_seg[1]]
             c['source_index'] = (src_seg[0], src_seg[1])
+        # source phrase from the alignments
+        elif 'alignments' in repr_dict:
+            alignments = []
+            for i in range(c['index'][0], c['index'][1]):
+                alignments.extend(c['alignments'][i])
+            alignments = set(alignments)
+            if len(alignments) == 0:
+                c['source_token'] = []
+                c['index'] = []
+            # source phrase -- substring between the 1st and the last word aligned to the target phrase
+            # (unaligned words in between are included)
+            else:
+                c['source_token'] = [repr_dict['source'][i] for i in alignments]
+                c['source_index'] = (alignments[0], alignments[-1] + 1)
+
         if len(c['token']) == 0:
             print("No token: from {} to {} in target: ".format(i, j), repr_dict['target'], repr_dict['source'], repr_dict['segmentation'])
-        c['index'] = [i, j]
         if j == 0:
             print("j==0!")
             print("Target: '{}', segmentation: {}, {}".format(' '.join(repr_dict['target'], i, j)))
+        if i == j or len(repr_dict['tags'][i:j]) == 0 or len(repr_dict['target'][i:j]) == 0:
+            print("i==j!")
+            print("Target: '{}', tags: '{}' segmentation: {}, {}".format(' '.join([w.encode('utf-8') for w in repr_dict['target']]), ' '.join(repr_dict['tags']), i, j))
+            print("Target segment: '{}', tag segment: '{}'".format(' '.join([w.encode('utf-8') for w in repr_dict['target'][i:j]]), ' '.join(repr_dict['tags'][i:j])))
         tags_cnt = Counter(repr_dict['tags'][i:j])
-        
+
         # super-pessimistic tagging -- if BAD occurs any number of times - the final tag is BAD
-        if all_bad:
+        if bad_tagging == "super_pessimistic":
             if tags_cnt['BAD'] > 0:
                 c['tag'] = 'BAD'
             else:
                 c['tag'] = 'OK'
-        # pessimistic tagging -- if BAD occurs more often or as much as OK -- the final tag is BAD
-        else:
-            if tags_cnt['OK'] > tags_cnt['BAD']:
+        # pessimistic tagging -- if BAD occurs in 1 of 3 words or more often -- the final tag is BAD
+        elif bad_tagging == "pessimistic":
+            if tags_cnt['BAD']/len(repr_dict['tags'][i:j]) < 0.3:
                 c['tag'] = 'OK'
             else:
                 c['tag'] = 'BAD'
+        # optimisic - if OK occurs as much or more than BAD - the final tag is OK
+        elif bad_tagging == "optimistic":
+            if tags_cnt['OK'] >= tags_cnt['BAD']:
+                c['tag'] = 'OK'
+            else:
+                c['tag'] = 'BAD'
+        else:
+            print("Unknown tag assignment scheme: {}".format(bad_tagging))
 
         for k in active_keys:
             c[k] = repr_dict[k]
         context_list.append(c)
-        print("New context: ", c)
     return context_list
 
 
@@ -157,7 +177,7 @@ def create_context_phrase(repr_dict, order=None, test=False, all_bad=False):
 # :order: -- order of ngram
 # :data_type: -- 'plain' - data is a flat list
 #                'sequential' - data is a list of sequences (used for dev and test)
-def create_contexts_ngram(data_obj, order=None, data_type='plain', test=False, all_bad=False):
+def create_contexts_ngram(data_obj, order=None, data_type='plain', test=False, only_target=False, bad_tagging="pessimistic"):
     '''
     :param data_obj: an object representing a dataset consisting of files
     :param data_type:
@@ -190,15 +210,15 @@ def create_contexts_ngram(data_obj, order=None, data_type='plain', test=False, a
 #        if type(data_obj[a_key]) is not list:
     if data_type == 'plain':
         for s_idx, sents in enumerate(zip(*data_obj.values())):
-            print("SENTENCE {}".format(s_idx))
-            cont = context_generator({data_obj.keys()[i]: sents[i] for i in range(len(sents))}, order, test=test, all_bad=all_bad)
+#            print("SENTENCE {}".format(s_idx))
+            cont = context_generator({data_obj.keys()[i]: sents[i] for i in range(len(sents))}, order, test=test, only_target=only_target, bad_tagging=bad_tagging)
             overall += len(cont)
             contexts.extend(cont)
             print("Contexts: {}".format(overall))
 #            print(contexts)
     elif data_type == 'sequential':
         for s_idx, sents in enumerate(zip(*data_obj.values())):
-            contexts.append(context_generator({data_obj.keys()[i]: sents[i] for i in range(len(sents))}, order, test=test, all_bad=all_bad))
+            contexts.append(context_generator({data_obj.keys()[i]: sents[i] for i in range(len(sents))}, order, test=test, only_target=only_target, bad_tagging=bad_tagging))
 
     return contexts
 
