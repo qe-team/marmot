f8b523eccb50ce6fa9bb44bb0c68a2218698c52c
varvara-l <varvara.logacheva@gmail.com>
2015-09-09 13:11:00 +0100

    added perplexity to LM features

--- marmot/features/phrase/lm_feature_extractor.py
+++ marmot/features/phrase/lm_feature_extractor.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import kenlm
 from marmot.features.feature_extractor import FeatureExtractor
 
@@ -8,7 +10,10 @@ class LMFeatureExtractor(FeatureExtractor):
         self.model = kenlm.LanguageModel(lm_file)
 
     def get_features(self, context_obj):
-        return [self.model.score(' '.join(context_obj['token']))]
+        log_prob = self.model.score(' '.join(context_obj['token']), bos=False, eos=False)
+        tg_len = len(context_obj['token'])
+        perplexity = 2**((-1/tg_len)*log_prob)
+        return [log_prob, perplexity]
 
     def get_feature_names(self):
-        return ['target_log_prob']
+        return ['target_log_prob', 'target_perplexity']
--- marmot/features/phrase/source_lm_feature_extractor.py
+++ marmot/features/phrase/source_lm_feature_extractor.py
@@ -8,10 +8,13 @@ class SourceLMFeatureExtractor(FeatureExtractor):
         self.model = kenlm.LanguageModel(lm_file)
 
     def get_features(self, context_obj):
-        if len(context_obj['source_token']) > 0:
-            return [self.model.score(' '.join(context_obj['source_token']))]
+        if 'source_token' in context_obj and len(context_obj['source_token']) > 0:
+            log_prob = self.model.score(' '.join(context_obj['source_token']), bos=False, eos=False)
+            src_len = len(context_obj['source_token'])
+            perplexity = 2**((-1/src_len)*log_prob)
+            return [log_prob, perplexity]
         else:
-            return [0.0]
+            return [0.0, 0.0]
 
     def get_feature_names(self):
-        return ['source_log_prob']
+        return ['source_log_prob', 'source_perplexity']
