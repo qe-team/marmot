44431f5bac5e8d48dc72c8af068ef2012327a704
varvara-l <varvara.logacheva@gmail.com>
2016-02-26 10:22:24 +0000

    segmentation from data in WMT16 format

---
+++ marmot/representations/segmentation_double_representation_generator.py
@@ -0,0 +1,57 @@
+from marmot.representations.representation_generator import RepresentationGenerator
+import codecs
+
+
+class SegmentationDoubleRepresentationGenerator(RepresentationGenerator):
+    '''
+    Both source and target are already segmented with '||'
+    '''
+
+    def get_segments_from_line(self, line):
+        seg = line.strip('\n').split(' || ')
+        cur_words, cur_seg = [], []
+        cur_pos = 0
+        for seg in line.strip('\n').split(' || '):
+            seg_split = seg.split()
+            cur_words.extend(seg_split)
+            cur_seg.append((cur_pos, cur_pos + len(seg_split)))
+            cur_pos += len(seg_split)
+        return cur_words, cur_seg
+
+    def parse_files(self, source_file, target_file, tags_file, phrase_align_file, word_align_file):
+        # extract source segments
+        with codecs.open(source_file, encoding='utf8') as source:
+            source_words, source_segments = [], []
+            for line in source:
+                cur_words, cur_seg = self.get_segments_from_line(line)
+                source_words.append(cur_words)
+                source_segments.append(cur_seg)
+
+        # reorder source segments
+        with codecs.open(phrase_align_file, encoding='utf8') as align:
+            source_segments_order = []
+            for idx, line in enumerate(align):
+                cur_seg = []
+                seg_num = [int(s) for s in line.strip('\n').split()]
+                for s in seg_num:
+                    cur_seg.append(source_segments[idx][s])
+                source_segments_order.append(cur_seg)
+
+        # extract target segments
+        with codecs.open(target_file, encoding='utf8') as target:
+            target_words, target_segments = [], []
+            for line in target:
+                cur_words, cur_seg = self.get_segments_from_line(line)
+                target_words.append(cur_words)
+                target_segments.append(cur_seg)
+
+        with codecs.open(tags_file, encoding='utf8') as tags:
+            phrase_tags = [line.split() for line in tags]
+
+        return {'segmentation': target_segments, 'source_segmentation': source_segments, 'source': source_words, 'target': target_words, 'alignments_file': word_align_file, 'tags': phrase_tags}
+
+    def __init__(self, source_file, target_file, tags_file, phrase_align_file, word_align_file):
+        self.data = self.parse_files(source_file, target_file, tags_file, phrase_align_file, word_align_file)
+
+    def generate(self, data_obj=None):
+        return self.data
