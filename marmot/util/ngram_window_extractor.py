5fe0deaf1141b8ec442c2ffca24eb0eed1e2e913
varvara-l <varvara.logacheva@gmail.com>
2016-02-26 10:06:32 +0000

    replace '_START_' with <s>

--- marmot/util/ngram_window_extractor.py
+++ marmot/util/ngram_window_extractor.py
@@ -37,7 +37,8 @@ def left_context(token_list, token, context_size=1, idx=None):
     if index != -1:
         for i in range(index-context_size, index):
             if i < 0:
-                left_window.append('_START_')
+#                left_window.append('_START_')
+                left_window.append('<s>')
             else:
                 left_window.append(token_list[i])
     return left_window
@@ -49,7 +50,8 @@ def right_context(token_list, token, context_size=1, idx=None):
     if index != -1:
         for i in range(index+1, index+context_size+1):
             if i > len(token_list)-1:
-                right_window.append('_END_')
+#                right_window.append('_END_')
+                right_window.append('</s>')
             else:
                 right_window.append(token_list[i])
     return right_window
