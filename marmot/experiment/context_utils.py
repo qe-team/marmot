86b3f810b1e09f7f63bd02f4837e51caa20a111a
varvara-l <varvara.logacheva@gmail.com>
2016-02-26 10:05:45 +0000

    process phrase-level tags

--- marmot/experiment/context_utils.py
+++ marmot/experiment/context_utils.py
@@ -97,12 +97,17 @@ def error_based_segmentation(repr_dict):
 # :only_target: -- True if only target sentence is segmented, needs to be processed without source segmentation
 # :bad_tagging: -- tag all phrases with at least one bad word as "BAD"
 #              if seg to False - only phrases with 50% or more bad words are tagged as "BAD"
-def create_context_phrase(repr_dict, order=None, unambiguous=False, test=False, bad_tagging="pessimistic"):
+# :tags_format: -- 'word': one tag per word (conversion to phrase-level tags needed),
+#                  'phrase': one tag per phrase (no conversion needed)
+def create_context_phrase(repr_dict, order=None, unambiguous=False, test=False, bad_tagging="pessimistic", tags_format='word'):
     '''
     :param repr_dict: a dict representing a 'line' or 'sentence' or a 'segment'
     :return: a list of context objects representing the data for each token in the sequence
     '''
-
+    #print("CONTEXT CREATOR for a sentence")
+    #print(repr_dict)
+    # how many words had to change their tag from good to bad and vise versa
+    good2bad, bad2good = 0, 0
     context_list = []
     # is checked before in create_contexts, but who knows
     if 'target' not in repr_dict:
@@ -118,13 +123,16 @@ def create_context_phrase(repr_dict, order=None, unambiguous=False, test=False, 
             repr_dict['segmentation'] = [(i, i+1) for i in range(len(repr_dict['target']))]
         # for the training data omitting sentences without segmentation
         else:
-            print("No 'segmentation' label in data representations")
+#            print("No 'segmentation' label in data representations")
             return []
 
     if unambiguous:
         assert('source_segmentation' not in repr_dict or len(repr_dict['source_segmentation']) == 0), "Error-based segmentation of target can't be performed if source segmentation exists -- after re-segmentation source and target segments won't match"
         assert(not test), "Error-based segmentation can't be applied to the test set"
+        #print("Unambiguous")
+        print("Old segmentation: ", repr_dict['segmentation'])
         repr_dict['segmentation'] = error_based_segmentation(repr_dict)
+        print("New segmentation: ", repr_dict['segmentation'])
 
     # no source segmentation means that no Moses segmentation was produced
     # in the training data we leave these sentences out
@@ -137,12 +145,9 @@ def create_context_phrase(repr_dict, order=None, unambiguous=False, test=False, 
         if len(repr_dict['source_segmentation']) != 0 and len(repr_dict['source_segmentation']) != len(repr_dict['segmentation']):
             print("Wrong segmentation lengths: ", repr_dict)
             sys.exit()
-    active_keys.remove('segmentation')
     for idx, (i, j) in enumerate(repr_dict['segmentation']):
-        print("Segment {}, ({}, {})".format(idx, i, j))
         c = {}
         c['token'] = repr_dict['target'][i:j]
-        print("Token: {}".format(c['token']))
         c['index'] = (i, j)
         # source phrase from the phrase segmentation
         if 'source_segmentation' in repr_dict and len(repr_dict['source_segmentation']) != 0:
@@ -154,7 +159,9 @@ def create_context_phrase(repr_dict, order=None, unambiguous=False, test=False, 
             alignments = []
             for ii in range(c['index'][0], c['index'][1]):
                 try:
-                    alignments.extend(repr_dict['alignments'][ii])
+                    cur_align = repr_dict['alignments'][ii]
+                    if cur_align is not None:
+                        alignments.append(repr_dict['alignments'][ii])
                 except IndexError:
                     print("Indices: {} to {}, current: {}".format(c['index'][0], c['index'][1], ii))
                     print("Alignments: ", repr_dict['alignments'])
@@ -180,39 +187,50 @@ def create_context_phrase(repr_dict, order=None, unambiguous=False, test=False, 
         if j == 0:
             print("j==0!")
             print("Target: '{}', segmentation: {}, {}".format(' '.join(repr_dict['target']), i, j))
-        if i == j or len(repr_dict['tags'][i:j]) == 0 or len(repr_dict['target'][i:j]) == 0:
+        if i == j or (len(repr_dict['tags'][i:j]) == 0 and tags_format == 'word') or len(repr_dict['target'][i:j]) == 0:
             print("i==j!")
             print("Target: '{}', tags: '{}' segmentation: {}, {}".format(' '.join([w.encode('utf-8') for w in repr_dict['target']]), ' '.join(repr_dict['tags']), i, j))
         tags_cnt = Counter(repr_dict['tags'][i:j])
 
         # super-pessimistic tagging -- if BAD occurs any number of times - the final tag is BAD
-        if bad_tagging == "super_pessimistic":
-            if tags_cnt['BAD'] > 0:
-                c['tag'] = 'BAD'
+        bad_all = tags_cnt['BAD']
+        good_all = tags_cnt['OK']
+        if tags_format == 'word':
+            if bad_tagging == "super_pessimistic":
+                if tags_cnt['BAD'] > 0:
+                    c['tag'] = 'BAD'
+                    good2bad += good_all
+                else:
+                    c['tag'] = 'OK'
+            # pessimistic tagging -- if BAD occurs in 1 of 3 words or more often -- the final tag is BAD
+            elif bad_tagging == "pessimistic":
+                if tags_cnt['BAD']/len(repr_dict['tags'][i:j]) < 0.3:
+                    c['tag'] = 'OK'
+                    bad2good += bad_all
+                else:
+                    c['tag'] = 'BAD'
+                    good2bad += good_all
+            # optimisic - if OK occurs as much or more than BAD - the final tag is OK
+            elif bad_tagging == "optimistic":
+                if tags_cnt['OK'] >= tags_cnt['BAD']:
+                    bad2good += bad_all
+                    c['tag'] = 'OK'
+                else:
+                    c['tag'] = 'BAD'
+                    good2bad += good_all
             else:
-                c['tag'] = 'OK'
-        # pessimistic tagging -- if BAD occurs in 1 of 3 words or more often -- the final tag is BAD
-        elif bad_tagging == "pessimistic":
-            if tags_cnt['BAD']/len(repr_dict['tags'][i:j]) < 0.3:
-                c['tag'] = 'OK'
-            else:
-                c['tag'] = 'BAD'
-        # optimisic - if OK occurs as much or more than BAD - the final tag is OK
-        elif bad_tagging == "optimistic":
-            if tags_cnt['OK'] >= tags_cnt['BAD']:
-                c['tag'] = 'OK'
-            else:
-                c['tag'] = 'BAD'
+                print("Unknown tag assignment scheme: {}".format(bad_tagging))
+                sys.exit()
+        elif tags_format == 'phrase':
+            c['tag'] = repr_dict['tags'][idx]
         else:
-            print("Unknown tag assignment scheme: {}".format(bad_tagging))
+            print("Unknown tags format: {}".format(tags_format))
+            sys.exit()
 
         for k in active_keys:
             c[k] = repr_dict[k]
-#        if len(c['source_token']) == 0 and not no_alignments:
-#            print("ERROR! Alignments exist, but source token is empty")
-#            sys.exit()
         context_list.append(c)
-    return context_list
+    return context_list, good2bad, bad2good
 
 
 # create contexts where 'token' is an ngram of arbitrary length
@@ -220,7 +238,7 @@ def create_context_phrase(repr_dict, order=None, unambiguous=False, test=False, 
 # :order: -- order of ngram
 # :data_type: -- 'plain' - data is a flat list
 #                'sequential' - data is a list of sequences (used for dev and test)
-def create_contexts_ngram(data_obj, order=None, data_type='plain', test=False, unambiguous=False, bad_tagging="pessimistic"):
+def create_contexts_ngram(data_obj, order=None, data_type='plain', test=False, unambiguous=False, bad_tagging="pessimistic", tags_format='word'):
     '''
     :param data_obj: an object representing a dataset consisting of files
     :param data_type:
@@ -250,6 +268,7 @@ def create_contexts_ngram(data_obj, order=None, data_type='plain', test=False, u
     if 'source_file' in data_obj:
         data_obj.pop('source_file')
     overall = 0
+    good2bad, bad2good = 0, 0
     if data_type == 'plain':
         print("DATATYPE: PLAIN")
         print(len(data_obj.values()))
@@ -257,14 +276,33 @@ def create_contexts_ngram(data_obj, order=None, data_type='plain', test=False, u
             print("{} --  {} values".format(r_key, len(data_obj[r_key])))
 #        print(zip(*data_obj.values())[0])
         for s_idx, sents in enumerate(zip(*data_obj.values())):
-#            print("SENTENCE {}".format(s_idx))
-            cont = context_generator({data_obj.keys()[i]: sents[i] for i in range(len(sents))}, order, test=test, unambiguous=unambiguous, bad_tagging=bad_tagging)
-            overall += len(cont)
-            contexts.extend(cont)
-            print("Contexts: {}".format(overall))
+            #print("SENTENCE {}".format(s_idx))
+#            all_out = create_context_phrase({data_obj.keys()[i]: sents[i] for i in range(len(sents))}, order, test=test, unambiguous=unambiguous, bad_tagging=bad_tagging)
+#            print("ALL: ", all_out)
+            #(cont, good, bad) = create_context_phrase({data_obj.keys()[i]: sents[i] for i in range(len(sents))}, order, test=test, unambiguous=unambiguous, bad_tagging=bad_tagging)
+            all_out = create_context_phrase({data_obj.keys()[i]: sents[i] for i in range(len(sents))}, order, test=test, unambiguous=unambiguous, bad_tagging=bad_tagging, tags_format=tags_format)
+            if len(all_out) < 3:
+                continue
+            good2bad += all_out[1]
+            bad2good += all_out[2]
+            overall += len(all_out[0])
+#            print("Type of the generated context: ", type(cont))
+            contexts.extend(all_out[0])
+#            print("Contexts: {}".format(overall))
     elif data_type == 'sequential':
+        print("SEQUENTIAL")
         for s_idx, sents in enumerate(zip(*data_obj.values())):
-            contexts.append(context_generator({data_obj.keys()[i]: sents[i] for i in range(len(sents))}, order, test=test, unambiguous=unambiguous, bad_tagging=bad_tagging))
+            all_out = create_context_phrase({data_obj.keys()[i]: sents[i] for i in range(len(sents))}, order, test=test, unambiguous=unambiguous, bad_tagging=bad_tagging, tags_format=tags_format)
+            if len(all_out) < 3:
+                continue
+            #print("SEQ CONTEXTS: {}".format(len(cont)))
+            good2bad += all_out[1]
+            bad2good += all_out[2]
+            overall += len(all_out[0])
+            contexts.append(all_out[0])
+    else:
+        print("UNKNOWN DATATYPE: {}".format(data_type))
+    print("Good to bad: {}\nBad to good: {}, \nTotal: {}".format(good2bad, bad2good, overall))
 
     return contexts
 
@@ -277,6 +315,8 @@ def get_contexts_words_number(contexts):
         try:
             numbers_list.append(len(c['token']))
         except TypeError:
+            print("Erroneous context: ", c)
+            print("List: ", contexts)
             print("The 'token' field has to be of type 'list', is actually {}".format(type(c['token'])))
             sys.exit()
     return numbers_list
